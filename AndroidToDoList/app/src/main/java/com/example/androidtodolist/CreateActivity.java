c3b8f2ae7547a4e3ca2826aba85f288e04acc53f
kina <khairinna@eass.com.my>
2019-04-15 15:01:49 +0800

    commit AndroidToDoList
    1st day workshop

---
+++ AndroidToDoList/app/src/main/java/com/example/androidtodolist/CreateActivity.java
@@ -0,0 +1,26 @@
+package com.example.androidtodolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class CreateActivity extends AppCompatActivity {
+
+    private Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
+                startActivity(listActivity);
+            }
+        });
+    }
+}
